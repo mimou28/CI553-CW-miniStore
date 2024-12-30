84c10bc5d6405a8e9492b7ddf0b67ef926ed4082
mimou28 <ayedim842@gmail.com>
2024-12-30 19:50:17 +0000

    Fix the quantity issue

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -9,12 +9,18 @@ import java.util.Collections;
  * @author  Your Name 
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
+public class BetterBasket extends Basket 
 {
-  private static final long serialVersionUID = 1L;
-  
-
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
+ @Override
+  public boolean add( Product pr )
+  {  
+	 for (Product prInList: this) {
+		 if (prInList.getProductNum().equals(pr.getProductNum())) {
+			 int quantity = pr.getQuantity()+prInList.getQuantity();
+			 prInList.setQuantity(quantity);
+			 return (true);
+		 }
+	 }
+    return super.add( pr );     // Call add in ArrayList
+  }
 }
